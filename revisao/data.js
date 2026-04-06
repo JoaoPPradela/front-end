0f3c619b820cad8632f6ef8bca8d50cb78c5d089
JoaoPPradela <joao.pradela@aluno.senai.br>
2026-04-06 16:49:16 -0300

    Primeiro commit

---
+++ revisao/data.js
@@ -0,0 +1,12 @@
+let dataAtual = new Date()
+let ano = dataAtual.getFullYear();
+let mes = dataAtual.getMonth() + 1;
+let dia = dataAtual.getDate();
+let hora = dataAtual.getHours();
+let minuto = dataAtual.getMinutes();
+let segundo = dataAtual.getSeconds();
+let fusorario = dataAtual.getTimezoneOffset();
+
+let formatador = new Intl.DateTimeFormat()
+
+console.log(`${dia < 10 ? `0${dia}` : dia }/${mes < 10 ? `0${mes}` : mes }/${ano} ${hora}:${minuto}:${segundo}:${fusorario}`)
